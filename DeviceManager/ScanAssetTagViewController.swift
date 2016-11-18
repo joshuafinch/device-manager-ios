0c772b1854e1b0d7300114a0118604f0eca3cb9a
Joshua Finch <joshua.finch@chelsea-apps.com>
2016-11-18 21:03:20 +0000

    Scan QR codes

--- DeviceManager/ScanAssetTagViewController.swift
+++ DeviceManager/ScanAssetTagViewController.swift
@@ -8,7 +8,97 @@
 
 import Foundation
 import UIKit
+import AVFoundation
 
-class ScanAssetTagViewController: UIViewController {
+class ScanAssetTagViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     
+    fileprivate var captureSession:AVCaptureSession?
+    fileprivate var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+    fileprivate var qrCodeFrameView:UIView?
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Get an instance of the AVCaptureDevice class to initialize a device object and provide the video
+        // as the media type parameter.
+        
+        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        do {
+            // Get an instance of the AVCaptureDeviceInput class using the previous device object.
+            let input: AVCaptureInput = try AVCaptureDeviceInput(device: captureDevice)
+            
+            // Initialize the captureSession object.
+            captureSession = AVCaptureSession()
+            // Set the input device on the capture session.
+            captureSession?.addInput(input)
+            
+            // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession?.addOutput(captureMetadataOutput)
+            
+            // Set delegate and use the default dispatch queue to execute the call back
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+            
+            // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            view.layer.addSublayer(videoPreviewLayer!)
+            
+            // Start video capture.
+            captureSession?.startRunning()
+            
+            // Move the message label to the top view
+            //view.bringSubviewToFront(messageLabel)
+            
+            // Initialize QR Code Frame to highlight the QR code
+            qrCodeFrameView = UIView()
+            qrCodeFrameView!.layer.borderColor = UIColor.green.cgColor
+            qrCodeFrameView!.layer.borderWidth = 2
+            view.addSubview(qrCodeFrameView!)
+            view.bringSubview(toFront: qrCodeFrameView!)
+        } catch {
+            print("Couldn't get capture device with input")
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        captureSession?.startRunning()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        captureSession?.stopRunning()
+    }
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+     
+        // Check if the metadataObjects array is not nil and it contains at least one object.
+        if metadataObjects == nil || metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            return
+        }
+        
+        // Get the metadata object.
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if metadataObj.type == AVMetadataObjectTypeQRCode {
+            // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj as AVMetadataMachineReadableCodeObject) as! AVMetadataMachineReadableCodeObject
+            qrCodeFrameView?.frame = barCodeObject.bounds;
+            
+            if metadataObj.stringValue != nil {
+                print("metadataObject: \(metadataObj.stringValue)")
+                
+                if metadataObj.stringValue.contains("https://assets.chelsea-apps.com/hardware"), let url = URL(string: metadataObj.stringValue) {
+                    UIApplication.shared.openURL(url)
+                }
+            }
+        }
+    }
 }
